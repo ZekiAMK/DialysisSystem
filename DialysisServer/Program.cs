5a952dbe4cde7b30a3f6885504fdb53e6e7eec5e
MesLice <timonplichta@gmail.com>
2026-03-20 09:40:11 +0100

    fixed target framework for dialysis server, added skeleton shared library for dtos

--- DialysisServer/Program.cs
+++ DialysisServer/Program.cs
@@ -3,12 +3,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=dialysis.sqlite"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+                       ?? "Data Source=dialysis.sqlite";
+
+builder.Services.AddDbContextPool<AppDbContext>(options =>
+{
+    options.UseSqlite(connectionString);
+    if (builder.Environment.IsDevelopment())
+    {
+        options.EnableSensitiveDataLogging();
+    }
+
+    // Use NoTracking by default for read-mostly queries; change per-query when updates are required
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+});
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+
+// Ensure DB is created and migrations are applied at startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.UseHttpsRedirection();
 
